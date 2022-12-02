97220027ceb4b000ba7f4f92346f5316b7809652
Adam Mizerski <amizerski@golem.network>
2022-12-02 11:21:23 +0100

    fix heimdall config

--- build/heimdall/scripts/entrypoint.sh
+++ build/heimdall/scripts/entrypoint.sh
@@ -25,13 +25,12 @@ then
     sed -i "s#^cors_allowed_origins.*#cors_allowed_origins = [\"*\"]#" config.toml
     # SEEDS
     sed -i "s#^seeds.*#seeds = \"${BOOTNODES:-"f4f605d60b8ffaaf15240564e58a81103510631c@159.203.9.164:26656,4fb1bc820088764a564d4f66bba1963d47d82329@44.232.55.71:26656,2eadba4be3ce47ac8db0a3538cb923b57b41c927@35.199.4.13:26656,3b23b20017a6f348d329c102ddc0088f0a10a444@35.221.13.28:26656,25f5f65a09c56e9f1d2d90618aa70cd358aa68da@35.230.116.151:26656"}\"#" config.toml
-    # heimdall-config.toml
     # BOR
-    sed -i "s#^bor_rpc_url.*#bor_rpc_url = \"http://bor:8545\"#" heimdall-config.toml
+    sed -i "s#^bor_rpc_url.*#bor_rpc_url = \"http://bor:8545\"#" config.toml
     # ETH1
-    sed -i "s#^eth_rpc_url.*#eth_rpc_url = \"${ETH1_RPC_URL}\"#" heimdall-config.toml
+    sed -i "s#^eth_rpc_url.*#eth_rpc_url = \"${ETH1_RPC_URL}\"#" config.toml
     # RABBITMQ
-    sed -i "s#^amqp_url.*#amqp_url = \"amqp://guest:guest@rabbitmq:5672\"#" heimdall-config.toml
+    sed -i "s#^amqp_url.*#amqp_url = \"amqp://guest:guest@rabbitmq:5672\"#" config.toml
 fi
 
 # If heimdalld container and we need to bootstrap on first run then download the snapshot
